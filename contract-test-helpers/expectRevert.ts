5d53746f42438ad7ef7cfd1d914c75931759e780
0x1ance <pktsangac@connect.ust.hk>
2023-01-30 12:48:12 +0800

    chore: remove unused log code

--- contract-test-helpers/expectRevert.ts
+++ contract-test-helpers/expectRevert.ts
@@ -2,7 +2,6 @@ import { assert } from "chai"
 
 export async function expectRevert(promise: Promise<any>, message: string) {
     await promise.then(() => assert.fail()).catch((err: any) => {
-        console.log('err.message: ',err.message)
         assert.include(err.message, message)
     })
 }
